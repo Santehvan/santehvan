e86588228c047c1a602bd742bbbebaedd91cab35
CopperGroup <coppergroupstudio@gmail.com>
2024-07-20 11:59:59 +0300

    Update InfoLinks.tsx

--- components/shared/InfoLinks.tsx
+++ components/shared/InfoLinks.tsx
@@ -86,7 +86,7 @@ const ListItem = React.forwardRef<
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
+          <div className="text-sm font-medium leading-none">{title}аіві</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
