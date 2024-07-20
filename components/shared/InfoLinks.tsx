3a3b36734cc9e3a59c4db944016e1ab567a27a0e
CopperGroup <coppergroupstudio@gmail.com>
2024-07-20 12:06:52 +0300

    Update InfoLinks.tsx

--- components/shared/InfoLinks.tsx
+++ components/shared/InfoLinks.tsx
@@ -77,9 +77,10 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <TransitionLink
+       <TransitionLink 
+      
           //@ts-ignore
-          href={ref}
+          href='/'
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
@@ -90,7 +91,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </TransitionLink>
+     </TransitionLink>
       </NavigationMenuLink>
     </li>
   )
