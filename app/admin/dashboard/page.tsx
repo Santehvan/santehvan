61ed780d270b56cbe7b529a29c2a3b10944fcc87
Santehvan <santehvan@gmail.com>
2024-07-27 14:58:50 +0300

    Update page.tsx

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,15 +1,15 @@
-// import Dashboard from "@/components/admin-components/Dashboard"
-// import { getDashboardData } from "@/lib/actions/order.actions";
+import Dashboard from "@/components/admin-components/Dashboard"
+import { getDashboardData } from "@/lib/actions/order.actions";
 
-// const Page = async () => {
-//     const dashboardData = await getDashboardData();
+const Page = async () => {
+    // const dashboardData = await getDashboardData();
 
-//     return (
-//         <section className="w-full px-10 py-20 h-screen overflow-hidden"> 
-//             <h1 className="w-full text-heading1-bold drop-shadow-text-blue max-[440px]:text-center">Dashboard</h1>
-//             <Dashboard stringifiedData={JSON.stringify(dashboardData)}/>
-//         </section>
-//     )
-// }
+    return (
+        <section className="w-full px-10 py-20 h-screen overflow-hidden"> 
+            {/* <h1 className="w-full text-heading1-bold drop-shadow-text-blue max-[440px]:text-center">Dashboard</h1>
+            <Dashboard stringifiedData={JSON.stringify(dashboardData)}/> */}
+        </section>
+    )
+}
 
-// export default Page;
+export default Page;
