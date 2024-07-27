7a56c16dc54183a53bf60dfdb0ee0994129d72b1
Santehvan <santehvan@gmail.com>
2024-07-27 16:24:06 +0300

    Update InfoLinks.tsx

--- components/shared/InfoLinks.tsx
+++ components/shared/InfoLinks.tsx
@@ -87,7 +87,7 @@ const ListItem = React.forwardRef<
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}аіві</div>
+          <div className="text-sm font-medium leading-none">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
