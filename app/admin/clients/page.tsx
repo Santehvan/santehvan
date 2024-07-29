4258096d0ed89e92066d17beafce36b62a8f2615
Santehvan <santehvan@gmail.com>
2024-07-29 16:54:46 +0300

    Update page.tsx

--- app/admin/clients/page.tsx
+++ app/admin/clients/page.tsx
@@ -1,16 +1,1 @@
-import { fetchUsers } from "@/lib/actions/user.actions";
-import { DataTable } from "./data-table";
-import { columns } from "./columns";
 
-const Page = async () => {
-
-    const users = await fetchUsers();
-    return (
-        <section className="w-full px-10 py-20 h-screen overflow-hidden"> 
-            <h1 className="w-full text-heading1-bold drop-shadow-text-blue max-[440px]:text-center">Клієнти</h1>
-            <DataTable columns={columns} data={users}/>
-        </section>
-    )
-}
-
-export default Page;
