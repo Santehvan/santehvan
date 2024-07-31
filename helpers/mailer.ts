742094758509a2c3ec3f419f7fe86020d6d85973
Santehvan <santehvan@gmail.com>
2024-07-31 14:13:22 +0300

    Update mailer.ts

--- helpers/mailer.ts
+++ helpers/mailer.ts
@@ -26,7 +26,6 @@ export const sendEmail = async({email, emailType, userId}:any) => {
         });
 
         const mailOptions = {
-            from: 'santehvan@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Лист підтвердження" : "Зміна пароля",
             text:emailType === "VERIFY" ?
