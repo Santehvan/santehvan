9acd7660b9a01f4af19ab301927eed04cf7b528c
R84N <misha3001011@gmail.com>
2024-07-31 22:20:24 +0300

    fsdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors:true
+  },
   images: {
       domains: ['shop.juventa.ua', 'utfs.io', 'uploadthing.com'], // Add the external hostname here
+  
     },
 };
 
