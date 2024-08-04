1fc4cd1f7eca19f761bdc67e4d279021c2844a22
Santehvan <santehvan@gmail.com>
2024-08-04 22:06:58 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript:{
-    ignoreBuildErrors:true
+  typescript: {
+    ignoreBuildErrors: true
   },
   images: {
-      domains: ['shop.juventa.ua', 'utfs.io', 'uploadthing.com'], // Add the external hostname here
-  
-    },
+    domains: ['shop.juventa.ua', 'utfs.io', 'uploadthing.com'], // Add the external hostname here
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Застосовується до всіх маршрутів
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
