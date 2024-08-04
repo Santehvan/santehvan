e07df278cb3a7f936c62330df55076c29c77655e
Santehvan <santehvan@gmail.com>
2024-08-04 23:26:57 +0300

    Update route.ts

--- app/api/getOrders/route.ts
+++ app/api/getOrders/route.ts
@@ -4,18 +4,28 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectToDB } from "@/lib/mongoose";
 import User from "@/lib/models/user.model";
 
+
+
+
+
 export async function GET(request: NextRequest) {
   try {
     await connectToDB();
 
     const orders = await Order.find({});
+    
 
     const response = NextResponse.json({
       message: "Orders found",
       data: orders,
     });
+
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+    response.headers.set('Surrogate-Control', 'no-store');
     
-    response.headers.set('Cache-Control', 'no-store');
+    
     return response;
   } catch (error: any) {
     console.log('Server Error:', error);
