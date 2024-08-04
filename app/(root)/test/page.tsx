f43e8f827eaabf4884e8a9053530c3aaa3b560dc
Santehvan <santehvan@gmail.com>
2024-08-04 20:35:06 +0300

    Update page.tsx

--- app/(root)/test/page.tsx
+++ app/(root)/test/page.tsx
@@ -1,13 +1,26 @@
-import Test from '@/components/shared/Test';
-import { fetchAllProducts } from '@/lib/actions/product.actions';
+'use client'
+
+
+import axios from 'axios';
 import React from 'react'
 
-const page = async ({searchParams}:any) => {
-    console.log('sd',searchParams)
-    let filtredProducts = await fetchAllProducts();
-    
+const page = () => {
+
+    const reloadOrders = async () => {
+        try {
+            const response = await axios.get("/api/getOrders");
+            console.log('response',response.data.data)
+           
+        } catch (error:any) {
+            console.log(error.mail);
+        }
+    }
+
   return (
-    <Test product={filtredProducts}/>
+    <div>
+        <button  onClick={reloadOrders}>fdsfsd</button>
+
+    </div>
   )
 }
 
