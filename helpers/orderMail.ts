f90dc19c32cda07fe5b3666295119a6eb4dabf45
Santehvan <santehvan@gmail.com>
2024-08-07 23:12:36 +0300

    Update orderMail.ts

--- helpers/orderMail.ts
+++ helpers/orderMail.ts
@@ -3,7 +3,7 @@ import bcryptjs from 'bcryptjs';
 import User from '@/lib/models/user.model';
 import { useToast } from "@/components/ui/use-toast"
 
-export const orderMail = async({orderId}:any) => {
+export const orderMail = async(orderId:any) => {
     try {
        
         
@@ -27,7 +27,12 @@ export const orderMail = async({orderId}:any) => {
             `
             <h1 style="font-family: sans-serif; margin-bottom:15px;">Нове замовлення було створене !</h1>
             <a style=" font-weight: bold; font-family: sans-serif;  color: #fff; text-decoration: none; background-color: #000; padding: 15px 30px; border-radius: 8px;" href="${process.env.DOMAIN}/${orderId}";>Переглянути </a>
-            <p style="font-family: sans-serif; padding:35px;>Натисніть на кнопку вище, щоб переглянути деталі замовлення</p>
+           <div style="font-size: 18px; padding-bottom: 40px; font-family: sans-serif;">
+                    <p style="padding-top: 40px;">Натисніть на кнопку вище щоб переглянути деталі замовлення.</p>
+                    <p>Якщо кнопка не спацювала перевірте замовлення в адмін панелі</a></p>
+                    <p>Дякуємо, що обрали SantehVan!</p>
+                   
+                </div>
             `
         }
 
